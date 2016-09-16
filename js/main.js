b1baa6b6efa3efd3242410d38172f8e6153b2b60
Alexandra <alexandrafernandezquispe@gmail.com>
2016-09-16 12:58:29 -0500

    subiendo bicis

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 function validateForm(){
-		/* Escribe tú código aquí */
 	var name = document.getElementById("name").value;
 	if (name == ""){
-		alert("llenar el campo nombre por favor");
+		alert("llenar el campo nombre");
 	} else if (/^[a-z]/.test(name.charAt(0))){
-		alert("Por favor ingrese la primera letra en mayuscula");
+		alert("ingrese la primera letra en mayuscula");
 	} else if (/^[0-9]/.test(name.charAt(0))){
 		alert("Por favor no ingrese numeros");
 	}
 
 	var lastname = document.getElementById("lastname").value;
 	if (lastname == ""){
-		alert("llenar el campo apellido por favor");
+		alert("llenar el campo apellido");
 	}
 
+	var correovalidado=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	var email = document.getElementById("input-email").value;
-	if (email == ""){
-		alert("llenar el campo correo por favor");
-	} else if (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email)) {
-		alert("correo invalido");
-	}
+	if (email == "" ){
+		alert("llenar el campo correo");
+	} else if(!correovalidado.test(email)) {
+        alert("Ingrese un correo válido.");
+    }
 
 	var password = document.getElementById("input-password").value;
 	if (password == "") {
-		alert("llenar el campo contraseña por favor");
+		alert("llenar el campo contraseña");
 	} else if (password.length < 6){ 
      	alert("Ingrese  al menos 6 caracteres"); 
      } else if (password == "123456" || password == "password" || password == "098754"){
@@ -32,7 +32,6 @@ function validateForm(){
 
     var menu = document.querySelector("select").value;
 	if (menu == 0) {
-		alert("selecciona un tipo de bici por favor");
-	}  
+		alert("selecciona un tipo de bici");
+	}    
 }
-		                  
